1c77875ea9638e658cb8e20ab9aee73217328da8
Oleg Grigorenko <grigorenko996@gmail.com>
2024-04-08 13:41:41 +0700

    Update script.js
    video playback update
    some screen size update

--- script.js
+++ script.js
@@ -1,94 +1,70 @@
 const menuButton = document.getElementById("menu-button");
 let menuOpened = false;
 const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
 
-
-if (windowWidth >= 985) {
-// opens Menu
-const openMenu = () => {
-    if (!menuOpened) {
-        document.querySelector("nav").style.display = "flex";
-        menuOpened = true;
-     } else {
-        document.querySelector("nav").style.display = "none";
-        menuOpened = false;
-     }; 
-};
-
-// cross/lines
-const changeSign = () => {
-    const topStick = document.getElementById("s1");
-    const midStick = document.getElementById("s2");
-    const bottomStick = document.getElementById("s3");
-    
-    if (menuOpened) {
-        topStick.style.transform = "rotateZ(45deg) translateY(7px) scaleX(120%)";
-        bottomStick.style.transform = "rotateZ(-45deg) translateY(-7px) scaleX(120%)";
-        midStick.style.display = "none";
-    } else {
-        topStick.style.transform = "rotateZ(0) translateY(0) scaleX(100%)";
-        bottomStick.style.transform = "rotateZ(0) translateY(0) scaleX(100%)";
-        midStick.style.display = "flex";
-    }
-};
-
-// listeners
-menuButton.addEventListener('click', openMenu);
-menuButton.addEventListener('click', changeSign);
-};
-
-if (windowWidth >= 1100) {
-
-    const pinkField = document.getElementById("pink");
-
-    const setWidth = () => {
-        const headerWidth = getComputedStyle(document.querySelector("header")).width;
-        pinkField.style.width = `calc(${headerWidth} + 2rem)`;
-        
+    if (windowWidth >= 900) {
+    // opens Menu
+    const openMenu = () => {
+        if (!menuOpened) {
+            document.querySelector("nav").style.display = "flex";
+            menuOpened = true;
+        } else {
+            document.querySelector("nav").style.display = "none";
+            menuOpened = false;
+        }; 
     };
 
-    const arrow = document.getElementById("arrow");
+    // cross/lines
+    const changeSign = () => {
+        const topStick = document.getElementById("s1");
+        const midStick = document.getElementById("s2");
+        const bottomStick = document.getElementById("s3");
+        
+        if (menuOpened) {
+            topStick.style.transform = "rotateZ(45deg) translateY(7px) scaleX(120%)";
+            bottomStick.style.transform = "rotateZ(-45deg) translateY(-7px) scaleX(120%)";
+            midStick.style.display = "none";
+        } else {
+            topStick.style.transform = "rotateZ(0) translateY(0) scaleX(100%)";
+            bottomStick.style.transform = "rotateZ(0) translateY(0) scaleX(100%)";
+            midStick.style.display = "flex";
+        }
+    };
 
-    window.addEventListener("scroll", () => {
-        arrow.style.display = "none";
-    });
+    // listeners
+    menuButton.addEventListener('click', openMenu);
+    menuButton.addEventListener('click', changeSign);
+    };
 
-    setWidth();
+    if (windowWidth >= 1100) {
 
-    const videoChatApp = document.getElementById("chatapp");
-    const videoGuard = document.getElementById("guard");
-    const videoDonut = document.getElementById("donut");
-    const hover1 = document.getElementById("hover1");
-    const hover2 = document.getElementById("hover2");
-    const hover3 = document.getElementById("hover3");
+        const pinkField = document.getElementById("pink");
 
-    videoChatApp.addEventListener("mouseover", () => {
-        videoChatApp.play();
-        hover1.style.display = "none";
-    });
+        const setWidth = () => {
+            const headerWidth = getComputedStyle(document.querySelector("header")).width;
+            pinkField.style.width = `calc(${headerWidth} + 2rem)`;
+            
+        };
 
-    videoGuard.addEventListener("mouseover", () => {
-        videoGuard.play();
-        hover2.style.display = "none";
-    });
+        const arrow = document.getElementById("arrow");
 
-    videoDonut.addEventListener("mouseover", () => {
-        videoDonut.play();
-        hover3.style.display = "none";
-    });
+        window.addEventListener("scroll", () => {
+            arrow.style.display = "none";
+        });
 
-    videoChatApp.addEventListener("mouseout", () => {
-        videoChatApp.pause();
-        hover1.style.display = "flex";
-    });
+        setWidth();
 
-    videoGuard.addEventListener("mouseout", () => {
-        videoGuard.pause();
-        hover2.style.display = "flex";
-    });
+        const videoChatApp = document.getElementById("chatapp");
+        const hover1 = document.getElementById("hover1");
 
-    videoDonut.addEventListener("mouseout", () => {
-        videoDonut.pause();
-        hover3.style.display = "flex";
-    });
-};
+        videoChatApp.addEventListener("mouseover", () => {
+            videoChatApp.play();
+            hover1.style.display = "none";
+        });
+
+        videoChatApp.addEventListener("mouseout", () => {
+            videoChatApp.pause();
+            hover1.style.display = "flex";
+        });
+    };
